934821989c67ada03e0cfe06a496e008903ea94a
Shadow <bartekkolosowski91@gmail.com>
2025-01-23 14:39:25 +0100

    Fixed: join voice channel

--- actions/join_voice_channel.js
+++ actions/join_voice_channel.js
@@ -29,20 +29,20 @@ module.exports = {
   // Action Meta Data
   //
   // Helps check for updates and provides info if a custom mod.
-  // If this is a third-party mod, please set "author" and "authorUrl".
-  //
-  // It's highly recommended "preciseCheck" is set to false for third-party mods.
-  // This will make it so the patch version (0.0.X) is not checked.
   //---------------------------------------------------------------------
 
-  meta: { version: "2.1.7", preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
+  meta: {
+    version: "2.1.7",
+    preciseCheck: true,
+    author: "Shadow",
+    authorUrl: null,
+    downloadUrl: null,
+  },
 
   //---------------------------------------------------------------------
   // Action Fields
   //
-  // These are the fields for the action. These fields are customized
-  // by creating elements with corresponding IDs in the HTML. These
-  // are also the names of the fields stored in the action's JSON data.
+  // These are the fields for the action.
   //---------------------------------------------------------------------
 
   fields: ["channel", "varName"],
@@ -52,10 +52,6 @@ module.exports = {
   //
   // This function returns a string containing the HTML used for
   // editing actions.
-  //
-  // The "isEvent" parameter will be true if this action is being used
-  // for an event. Due to their nature, events lack certain information,
-  // so edit the HTML to reflect this.
   //---------------------------------------------------------------------
 
   html(isEvent, data) {
@@ -66,8 +62,7 @@ module.exports = {
   // Action Editor Init Code
   //
   // When the HTML is first applied to the action editor, this code
-  // is also run. This helps add modifications or setup reactionary
-  // functions for the DOM elements.
+  // is also run.
   //---------------------------------------------------------------------
 
   init() {},
@@ -76,17 +71,27 @@ module.exports = {
   // Action Bot Function
   //
   // This is the function for the action within the Bot's Action class.
-  // Keep in mind event calls won't have access to the "msg" parameter,
-  // so be sure to provide checks for variable existence.
   //---------------------------------------------------------------------
 
   async action(cache) {
+    const { joinVoiceChannel } = require("@discordjs/voice");
     const data = cache.actions[cache.index];
-    const Audio = this.getDBM().Audio;
+    const channel = await this.getVoiceChannelFromData(
+      data.channel,
+      data.varName,
+      cache
+    );
 
-    const channel = await this.getVoiceChannelFromData(data.channel, data.varName, cache);
     if (channel) {
-      Audio.connectToVoice(channel);
+      try {
+        joinVoiceChannel({
+          channelId: channel.id,
+          guildId: channel.guild.id,
+          adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+      } catch (error) {
+        console.error("Error joining voice channel:", error);
+      }
     }
 
     this.callNextAction(cache);
@@ -103,10 +108,7 @@ module.exports = {
   //---------------------------------------------------------------------
   // Action Bot Mod
   //
-  // Upon initialization of the bot, this code is run. Using the bot's
-  // DBM namespace, one can add/modify existing functions if necessary.
-  // In order to reduce conflicts between mods, be sure to alias
-  // functions you wish to overwrite.
+  // Upon initialization of the bot, this code is run.
   //---------------------------------------------------------------------
 
   mod() {},
